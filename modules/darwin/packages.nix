cea71d7d6d55d32076c6f9452e3cb3b8146a1028
Ludovic Vannoorenberghe <ludovic.v@me.com>
2025-08-11 14:53:27 +0100

    add lazygit claude-code and devbox

--- modules/darwin/packages.nix
+++ modules/darwin/packages.nix
@@ -1,7 +1,11 @@
-{ pkgs }:
+{ pkgs, pkgs-unstable, ... }:
 
 with pkgs;
 let shared-packages = import ../shared/packages.nix { inherit pkgs; }; in
 shared-packages ++ [
   dockutil
-]
+  devbox
+  lazygit
+] ++ (with pkgs-unstable; [
+  claude-code
+])
